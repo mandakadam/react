54f49fdf4e59d2260d7c279f61e068948b361543
Manda <mandakadam@gmail.com>
2020-03-01 00:29:10 +0530

    Build Folder Added

---
+++ build/precache-manifest.c1f49f2eea3572fd3a15340a91a4dc47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a0edb5c137ff8745615165d779b84d8",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "1f29b98db42ae666298b",
+    "url": "/react-calculator/static/css/main.8edd0094.chunk.css"
+  },
+  {
+    "revision": "9480d6f9b08deafbadf9",
+    "url": "/react-calculator/static/js/2.bc725ee2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-calculator/static/js/2.bc725ee2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f29b98db42ae666298b",
+    "url": "/react-calculator/static/js/main.8f6fb7fa.chunk.js"
+  },
+  {
+    "revision": "48aef87c5aa6ecaf3eb9",
+    "url": "/react-calculator/static/js/runtime-main.0e4794dd.js"
+  }
+]);
